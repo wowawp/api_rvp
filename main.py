ff5711a1b4ff49d406315febb0bf8b056fd19d82
wowawp <nYAVeQHjW7e4ffw>
2021-09-22 18:31:07 +0300

    add todo

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# TODO 1) Подключиться через сокет к существующему api РВП.
+# TODO 2) Послать запрос по api на получения потока данных о нахождении поезда на рельсовых цепях.
+# TODO 3) Получить ответ от сервера.
+# TODO 4) Обработать полученный ответ.
+# TODO 5) Записать обработанный ответ в ексель таблицу/ django
 import socket
 import sys
 import json
@@ -10,8 +15,6 @@ server_address = ('10.238.3.101', 2194)
 print(sys.stderr, 'connecting to %s port %s' % server_address)
 sock.connect(server_address)
 try:
-    # Send data
-
     # j = {"jsonrpc": "2.0","method": "listLines","id": 1}
     # query = '{"jsonrpc": "2.0","method": "listLines","id": 1}'
     request = {'jsonrpc': '2.0', 'method': 'addListener', 'params':  {'lineIds': ["sllLine"], 'intervalMsecs': 1000}, 'id': 1}
@@ -26,7 +29,7 @@ try:
         with open('line.txt', 'a') as file:
             file.write(data.decode('utf-8'))
         a = data.decode('utf-8')
-        da = json.loads(a)
+        # da = json.loads(a)
         # if 'name' in da['items'] and da['items']['name'].lower() == 'scarf':
         #     print'There is Scarf'
         print('received "%s"' % data.decode('utf-8'))
