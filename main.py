f944e15080b2a994d69ca2f269eaa33e756bd0d6
wowawp <nYAVeQHjW7e4ffw>
2021-09-30 03:19:56 +0300

    add check approved from server

--- main.py
+++ main.py
@@ -4,36 +4,40 @@
 # TODO 4) Обработать полученный ответ.
 # TODO 5) Записать обработанный ответ в ексель таблицу/ django
 import socket
-import sys
-import json
-import time
-# from csv_file import *
+from csv_file import *
 
-dict1 = []
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen = True
-
-server_address = ('10.238.3.101', 2194)
-print(sys.stderr, 'connecting to %s port %s' % server_address)
-sock.connect(server_address)
-try:
-    request = {'jsonrpc': '2.0',
+request = {'jsonrpc': '2.0',
                'method': 'addListener',
-               'params':  {'lineIds': ["sllLine"],
+               'params':  {'lineIds': ['sllLine'],
                            'intervalMsecs': 1000},
                'id': 1}
+approved = {'jsonrpc': '2.0',
+            'result': {},
+            'id': 1}
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('10.238.3.101', 2194)
+sock.connect(server_address)
+print('connected to %s port %s' % server_address)
+listen = True
+
+try:
     request = json.dumps(request)
+    approved = json.dumps(approved)
     print('sending "%s"' % request)
     sock.sendall(bytes(request, encoding="utf-8"))
     while listen:
-        time.sleep(1)
+        # time.sleep(1)
         data = sock.recv(16384)
-        with open('json.json', 'w') as file:
-            file.write(data.decode('utf-8'))
-        a = data.decode('utf-8')
-        # csv_file()
-        print('received "%s"' % data.decode('utf-8'))
+        sock.sendall(bytes(approved, encoding="utf-8"))
+        if len(data) == 0:
+            print('WTF!!!')
+        else:
+            with open('json.json', 'w') as file:
+                file.write(data.decode('utf-8'))
+                a = data.decode('utf-8')
+                # csv_file()
+                print('received "%s"' % data.decode('utf-8'))
 finally:
     print('closing socket')
     sock.close()
